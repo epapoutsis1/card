9dea56f06fdf532ba8ca186ff4920d419e191529
Evangelos Papoutsis <epapoutsis@umassd.edu>
2025-02-20 15:24:11 -0500

    Animation JavaScript & CSS

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function animation() {
     const card = document.querySelector('.logoFinal');
-    card.style.height = '100px';
-    card.style.width = '100px';
+    card.style.height = '300px';
+    card.style.width = '300px';
     card.style.transition = 'all 1s ease-in-out';
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+    const card = document.querySelector('.card');
+    card.classList.add('zoomInDown');
+});
+
+document.card.addEventListener("DOMContentLoaded", function() {
     const card = document.querySelector('.card');
     card.classList.add('zoomInDown');
 });
